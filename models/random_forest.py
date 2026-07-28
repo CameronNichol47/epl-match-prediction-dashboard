6dfb0c50e74f5d5e48df852105bda8370bfd3718
Cameron Nichol <99camnichol21@gmail.com>
2026-07-28 17:04:32 -0400

    Add Random Forest baseline and evaluation

---
+++ models/random_forest.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+
+BASE_DIR = Path(__file__).resolve().parent
+PROJECT_DIR = BASE_DIR.parent
+
+df = pd.read_csv(PROJECT_DIR / "data" / "training" / "training_dataset.csv")
+
+X = df.drop(columns=["Date","Home","Away","Result"])
+y = df["Result"]
+
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+#Doing chronological split: 
+split = int(len(df) * 0.8)
+
+X_train = X.iloc[:split]
+X_test = X.iloc[split:]
+
+y_train = y.iloc[:split]
+y_test = y.iloc[split:]
+
+model = RandomForestClassifier(n_estimators=100, max_depth=None, random_state=42)
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, predictions))
+print(confusion_matrix(y_test, predictions))
+print(classification_report(y_test, predictions))
